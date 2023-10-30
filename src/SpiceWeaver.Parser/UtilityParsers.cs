c19b8c528999c78ee68a6892b6c5f99d64af0a9f
Ben Simpson <ben@mrflux.net.nz>
2023-10-30 20:17:03 +1300

    Skip comments instead of stripping them

--- src/SpiceWeaver.Parser/UtilityParsers.cs
+++ src/SpiceWeaver.Parser/UtilityParsers.cs
@@ -1,9 +1,10 @@
 ﻿using Pidgin;
+using Pidgin.Comment;
 using static Pidgin.Parser;
 
 namespace SpiceWeaver.Parser;
 
-internal static class Tokens
+internal static class UtilityParsers
 {
     public static readonly Parser<char, char> OpenBrace = Tok('{');
     public static readonly Parser<char, char> CloseBrace = Tok('}');
@@ -21,11 +22,20 @@ internal static class Tokens
                 .Assert(s => s.Length <= 64 && !s.EndsWith('_'))
         );
 
+    public static readonly Parser<char, Unit> SkipWhiteSpacesAndComments =
+        OneOf(
+                CommentParser.SkipLineComment(Try(String("//"))).Before(SkipWhitespaces),
+                CommentParser.SkipBlockComment(Try(String("/*")), String("*/")).Before(SkipWhitespaces)
+            )
+            .SkipMany()
+            .Between(SkipWhitespaces);
+
+
     public static Parser<char, string> Keyword(string keyword) => Tok(keyword);
 
     // https://www.benjamin.pizza/posts/2019-12-08-parsing-prolog-with-pidgin.html
     // Replace with built-in version when available
-    public static Parser<char, T> Tok<T>(Parser<char, T> p) => Try(p).Before(SkipWhitespaces);
+    public static Parser<char, T> Tok<T>(Parser<char, T> p) => Try(p).Before(Rec(() => SkipWhiteSpacesAndComments));
     public static Parser<char, char> Tok(char value) => Tok(Char(value));
     public static Parser<char, string> Tok(string value) => Tok(String(value));
 }
